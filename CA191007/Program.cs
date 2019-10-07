58e3b76596cab374100517b6a7fe9a4d8fd9e750
Juhasz Zoltan <juhaszzoltanmate@yahoo.com>
2019-10-07 19:16:09 +0200

    Add project files.

---
+++ CA191007/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA191007
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lista = new List<string>
+            {
+                "borsó", "lovasszekér", "kréta"
+            };
+
+            foreach (var i in lista)
+            {
+                Console.Write(i);
+            }
+        }
+    }
+}
